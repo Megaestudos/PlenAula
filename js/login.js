5f417fb2691729660f7aa10b85b6cde357e362e2
Megaestudos <lomateco@gmail.com>
2026-04-18 13:24:27 -0300

    login.js

--- js/login.js
+++ js/login.js
@@ -6,9 +6,18 @@ document.addEventListener("DOMContentLoaded", () => {
             btn.innerHTML = '<i class="ph ph-spinner ph-spin"></i> Conectando...';
             btn.disabled = true;
             loginWithGoogle().catch(() => {
-                btn.innerHTML = '<i class="ph-fill ph-google-logo"></i> Entrar com Google';
+                btn.innerHTML = '<img src="https://fonts.gstatic.com/s/i/productlogos/googleg/v6/24px.svg" style="width: 18px;" /> Entrar com Google';
                 btn.disabled = false;
             });
         });
     });
 });
+
+window.openLoginModal = function() {
+    const modal = document.getElementById('loginModalOverlay');
+    if (modal) modal.classList.add('show');
+}
+window.closeLoginModal = function() {
+    const modal = document.getElementById('loginModalOverlay');
+    if (modal) modal.classList.remove('show');
+}
